2f53a16055c151f56ce57fd89e3105328bb476a9
Carlos Corbacho <carlos@strangeworlds.co.uk>
2013-02-24 08:50:37 +0000

    Add start of an instruction parser
    Other than printing out the argumens for 'add', it doesn't do much.

--- interpreter.scala
+++ interpreter.scala
@@ -1,8 +1,30 @@
+class Parser {
+  val dispatcher = collection.immutable.HashMap("add" -> add _)
+
+  def parse(s: String) {
+    val array = s.split(" ")
+    if (array(0) == "") {
+      throw new Exception("Invalid instruction")
+    }
+    val handler = dispatcher.get(array(0))
+    handler match {
+      case None => throw new Exception("Unknown instruction")
+      case Some(f) => f(array.drop(1))
+    }
+  }
+
+  def add(args: Array[String]) {
+    println(args mkString " ")
+  }
+}
+
+
 object Interpreter {
   def main(args: Array[String]) {
+    val parser = new Parser
     while (true) {
       val ln = Console.readLine()
-      println(ln)
+      parser.parse(ln)
     }
   }
 }
